5e80de40170605d5551b3704a1fc2a5fb81e6354
tiptondt1998 <dylan.tipton@live.com>
2023-03-14 12:23:45 -0400

    Add files via upload

--- demo.js
+++ demo.js
@@ -46,3 +46,33 @@ nVal.addEventListener("click", function(){
     var l = window.prompt("enter a value");
     window.alert(l);
 });
+
+/*
+ IMORTANT: Put the script call at the bottom of the html file. They way that the program will load into memory is that the html document will go line by line, see the script file, then
+ load the script into memory before continuing with the html document. If the script trys to load an html element that hasn't finished loading, then the associated functionality will have
+ a null pointer error, meaning that the function returns no value due to the input being null.
+ */
+
+
+/*
+
+This is a loop. These will, as their name suggests, loop through an element in order to modify data. You can use it to iterate over things like arrays or lists, or to modify a value until
+it is usable.
+
+*/
+
+loop_arr = { 'string1', 'string2', 'string3' };
+
+//arrays begin at 0, so string1 is loop_arr at 0, or loop_arr[0]
+
+
+
+for (i = 0; i < loop_arr.length; i++) { //initializes the loop to the first element of the array, tells the loop to run as long as i is less than the length of the array, 
+                                        //increments i after evaluation
+
+    if (loop_arr[i] == 'string2') {// if the array at the value of i is equal to string2, do what's inside of the if statement
+       console.log('string2');
+        break; //keyword that tells the loop to stop. There is also the keyword continue, which tells the loop to go to the next iteration
+    }
+
+}
